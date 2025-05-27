0b565aa5f843c680ff69c9c9ad8765c1f52e457d
Purva-Pathania <pathaniapurva40@gmail.com>
2025-05-27 22:02:04 +0530

    Update helper.js

--- server/utilities/helper.js
+++ server/utilities/helper.js
@@ -1,9 +1,9 @@
 const cloudinary=require("cloudinary").v2
 
 cloudinary.config({
-    cloud_name:process.env.CLOUD_name,
-    api_key:process.env.API_key,
-    api_secret:process.env.API_secret
+    cloud_name:process.env.CLOUD_NAME,
+    api_key:process.env.API_KEY,
+    api_secret:process.env.API_SECRET
 })
 
 const uploadImg=(fileBuffer,publicId)=>{
